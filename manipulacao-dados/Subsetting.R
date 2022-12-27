e739ec7729633d65689beb859d5131452e339d6e
João Cshunderlick Neto <joaocshunderlick26@gmail.com>
2022-12-26 21:01:57 -0300

    manipulacao-dados/subsetting: criando exemplos

---
+++ manipulacao-dados/Subsetting.R
@@ -0,0 +1,38 @@
+# 01 - Manipulando dados com subsetting
+
+# Configurando o diretório de trabalho
+setwd('C:/Users/Joao/Development/Apostila/manipulacao-dados')
+getwd()
+
+# Desta vez iremos utilizar um dataset retirado do site Kaggle,
+# que se trata de uma base de dados com os jogadores presentes no
+# jogo eletrônico da Eletronic Arts, FIFA 23.
+# O dataset pode ser obtido neste link:
+# https://www.kaggle.com/datasets/sanjeetsinghnaik/fifa-23-players-dataset?select=Fifa+23+Players+Data.csv
+
+# Carregando o dataset
+players <- read.csv('../datasets/fifa23_players_data.csv')
+View(players)
+str(players)
+
+# Selecionando colunas para visualizar a partir de um vetor
+fields <- c("Full.Name", "Value.in.Euro.", "Positions.Played", "Nationality", "Club.Name")
+View(players[fields],)
+
+# Removendo colunas ao visualizar um data frame
+View(players[-1, -3])
+
+# Buscando a 5 primeiras observações
+View(players[1:5,])
+
+# Inserindo filtros
+View(players[which(players$Nationality == 'Brazil'),])
+
+# Criando "subset" do data frame principal
+brazilianPlayers <- players[which(players$Nationality == 'Brazil'),]
+View(brazilianPlayers)
+
+# Utilizando a função subset
+View(subset(players, players$Nationality == 'Brazil', select = fields))
+
+
