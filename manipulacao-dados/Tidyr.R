013ba3fa2f2e0f47500d0b83dde10087c435bff3
João Cshunderlick Neto <joaocshunderlick26@gmail.com>
2022-12-30 14:20:53 -0300

    manipulacao-dados/tidyr: criando exemplos

---
+++ manipulacao-dados/Tidyr.R
@@ -0,0 +1,83 @@
+# 03 - Transformando dados com tidyr
+
+# Configurando o diretório de trabalho
+setwd('C:/Users/Joao/Development/Apostila/manipulacao-dados')
+getwd()
+
+# Instalando os pacotes
+install.packages('tidyr')
+library(tidyr)
+library(dplyr)
+
+# Reshape dos dados
+numberId <- c(3, 1, 2, 5, 4)
+name <- c('Carlos', 'Ana', 'Augusto', 'Sérgio', 'Letícia')
+timeExecution1 <- c(1.38, 1.45, 1.39, 1.41, 1.37)
+timeExecution2 <- c(1.39, 1.41, 1.41, 1.39, 1.40)
+
+swimmingTimes <- data.frame(numberId, name, timeExecution1, timeExecution2)
+swimmingTimes
+
+# Juntando as colunas de tempo em uma só
+swimmingTimes %>%
+  pivot_longer(timeExecution1:timeExecution2,
+               names_to = "Description",
+               values_to = "TimeExecution") %>%
+  arrange(TimeExecution)
+
+# Desfazendo o passo anterior
+swimmingTimes %>%
+  pivot_longer(timeExecution1:timeExecution2,
+               names_to = "Description",
+               values_to = "TimeExecution") %>%
+  pivot_wider(names_from = "Description",
+              values_from = "TimeExecution")
+
+# Carregando o dataset
+players <- read.csv('../datasets/fifa23_players_data.csv')
+View(players)
+
+# Separando as linhas em colunas
+players %>%
+  separate(
+    col = Positions.Played,
+    into = c("Position1", "Position2"),
+    sep = ","
+  ) %>%
+  select(Known.As, Position1, Position2)
+
+# Juntando os dados novamente
+players %>%
+  separate(
+    col = Positions.Played,
+    into = c("Position1", "Position2"),
+    sep = ","
+  ) %>%
+  unite(Position1, Position2, col = "Positions.Played", sep = "|") %>%
+  select(Known.As, Positions.Played)
+
+# Resolvendo o problema dos valores 0 no exemplo passado
+
+# Removendo os dados desnecessários para o propósito
+data <- players %>% 
+  select(Known.As, Value.in.Euro., Positions.Played, Nationality, Club.Name)
+data[1,]
+
+# 1° Passo - Transformar os 0 em valores NA
+data$Value.in.Euro. <- na_if(data$Value.in.Euro., 0)
+View(data)
+
+# 2° Passo - Removendo os valores NA do data frame
+data <- data %>% drop_na()
+
+# 3° Passo - Realizando o agrupamento novamente
+playersValueSummary <- data %>% group_by(Nationality) %>%
+  summarize(
+    meanValue = mean(Value.in.Euro.),
+    biggestValue = max(Value.in.Euro.),
+    lowestValue = min(Value.in.Euro.),
+    playersPerNationality = n()
+  ) %>%
+  arrange(desc(playersPerNationality))
+
+View(playersValueSummary)
